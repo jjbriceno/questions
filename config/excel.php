7d334a3642cf42e9157b70af26dbabd9d07f69a5
José Briceño <josejorgebriceno9@gmail.com>
2023-07-26 14:46:13 -0400

    Fixed excel compatibility.

--- config/excel.php
+++ config/excel.php
@@ -47,7 +47,7 @@ return [
             'line_ending'            => PHP_EOL,
             'use_bom'                => false,
             'include_separator_line' => false,
-            'excel_compatibility'    => true,
+            'excel_compatibility'    => false,
             'output_encoding'        => '',
             'test_auto_detect'       => true,
         ],
