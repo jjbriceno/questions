29a1e883bea5906225a4f19ea3c483546ead3776
Francisco J. P. Ruiz <javierrupe19@gmail.com>
2023-07-29 18:04:25 -0400

    minor fix

--- resources/js/components/PresentationComponent.vue
+++ resources/js/components/PresentationComponent.vue
@@ -2,7 +2,7 @@
     <div class="col-md-6">
         <!-- Section 1 -->
         <!-- ... Content for Section 1 ... -->
-        <h1 class="text-center">Bienvenido a la recolección de datos de los estudiantes de primaria</h1>
+        <h1 class="text-center">Bienvenido al Cuestionario de Matemáticas para la recolección de datos de los estudiantes de primaria</h1>
         <div class="d-flex justify-content-center mt-3">
             <button class="btn btn-primary mx-2" @click="checkPersonalInfo">Ir al cuestionario</button>
             <button class="btn btn-primary mx-2" @click="adminLogin">Entrar al sistema como Admin</button>
